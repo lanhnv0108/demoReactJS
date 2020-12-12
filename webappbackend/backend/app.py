d58b47bbf9897450521f39a4da8f255d89c4fd29
lanhnv0108 <lanhnv.0108@gmail.com>
2020-12-12 13:03:21 +0700

    change app.py

--- webappbackend/backend/app.py
+++ webappbackend/backend/app.py
@@ -11,17 +11,19 @@ ConnectionData['user'] = 'postgres'
 ConnectionData['password'] = 'postgres'
 ConnectionData['host'] = str(db_ip)
 ConnectionData['port'] = '5432'
-ConnectionData['database'] = 'northwind'
+ConnectionData['database'] = 'thigiuaki'
 
 @app.route('/')
 def hello():    
-    return 'this is backend'
+    #return 'this is backend'
+    c1 = bo.Customer(1, 'DAU xanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
+    return c1.CustomerName
 
 @app.route('/test_insert')
 def test_insert():
     #ConnectionString = 'database=northwind user=postgres password=postgres host=10.0.2.15 port=5432'
     c2 = do.Customer(ConnectionData)
-    c1 = bo.Customer(1, 'DAU xanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
+    c1 = bo.Customer(1, 'Lanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
     s1 = c2.insert(c1)
     return s1
 
