ab96889cffef95de358dc24162763b3419e31954
lanhnv0108 <lanhnv.0108@gmail.com>
2020-12-18 23:01:01 +0700

    add update function

--- webappbackend/backend/DataObjects.py
+++ webappbackend/backend/DataObjects.py
@@ -1,5 +1,6 @@
 import psycopg2
 from BusinessObjects import Customer as CustomerEntity
+
 class Customer:
     def __init__(self, ConnectionData):
         self.ConnectionData = ConnectionData
@@ -82,7 +83,7 @@ class Customer:
                                   port=self.ConnectionData['port'],
                                   database=self.ConnectionData['database'])
             cur = con.cursor()
-            sql = "DELETE * FROM TblCustomers WHERE CustomerID = %s"
+            sql = "DELETE FROM TblCustomers WHERE CustomerID = %s"
             cur.execute(sql, (customer.CustomerID,))
             con.commit()
             row = cur.rowcount
@@ -95,6 +96,30 @@ class Customer:
         finally:
             if con is not None:
                 con.close()
+
+    def update(self , customer : CustomerEntity):
+        con = None
+        try:
+            con = psycopg2.connect(user=self.ConnectionData['user'],
+                                  password=self.ConnectionData['password'],
+                                  host=self.ConnectionData['host'],
+                                  port=self.ConnectionData['port'],
+                                  database=self.ConnectionData['database'])
+            cur = con.cursor()
+            sql = "UPDATE TblCustomers CustomerName = %s, ContactName = %s, Address = %s, City = %s, PostalCode = %s, Country = %s) WHERE CustomerID = %s"
+            cur.execute(sql, (customer.CustomerID, customer.CustomerName, customer.ContactName, customer.Address, customer.City,customer.PostalCode, customer.Country))
+            con.commit()
+            row = cur.rowcount
+            if row > 0:
+                return 'Updated customer' , 200
+            con.close()
+            return 'Customer ID not found' ,200
+        except (Exception, psycopg2.DatabaseError) as error:
+            return str(error)
+        finally:
+            if con is not None:
+                con.close()
+# -------------------------------------------------------
 
 
 if __name__ == "__main__":
--- webappbackend/backend/app.py
+++ webappbackend/backend/app.py
@@ -19,13 +19,23 @@ def hello():
     c1 = bo.Customer(1, 'DAU xanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
     return c1.CustomerName
 
-@app.route('/test_insert')
+@app.route('/customer_insert')
 def test_insert():
     #ConnectionString = 'database=northwind user=postgres password=postgres host=10.0.2.15 port=5432'
     c2 = do.Customer(ConnectionData)
-    c1 = bo.Customer(1, 'Lanh', 'Peter', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
+    c1 = bo.Customer(1, 'Lanh', 'Nguyen', '566 Nui Thanh', 'Danang', '50000', 'Vietnam')
     s1 = c2.insert(c1)
     return s1
+
+@app.route('/user/insert', methods=['POST'])
+def user_insert():
+    data = request.json
+    c1 = bo.Customer(data['CustomerID'], data['CustomerName'], data['ContactName'], data['Address'], data['City'], data['PostalCode'], data['Country'])
+    c2 = do.Customer(ConnectionData)
+    s1 = c2.insert(c1)
+    result = {}
+    result['message'] = s1
+    return jsonify(result), 200
 
 @app.route('/test_send_receive', methods=['POST'])
 def test_send_receive():
@@ -36,20 +46,27 @@ def test_send_receive():
     return jsonify(result), 200
 
 #Show all item from Customer
-@app.route('/user/all_Customer')
+@app.route('/customer/all_Customer')
 def get_all_user():
     result = do.Customer(ConnectionData).get_all()
     return jsonify(result), 200
-
-@app.route('/user/delete/<int:customer_id>' , methods=['PUT'])
+# Delete customer
+@app.route('/customer/delete/<int:customer_id>' , methods=['DELETE'])
 def delete_user_by_id(customer_id):
     c = bo.Customer(CustomerID = customer_id)
     result = do.Customer(ConnectionData).delete(c)
     return jsonify({'message': result[0]}),result[1]
+#  Update customer
 
+@app.route('/customer/update/<int:customer_id>' , methods=['PUT'])
+def update_by_id(customer_id):
+    data = request.json
+    c = bo.Customer(CustomerID = data['customer_id'], CustomerName=data['CustomerName'], ContactName=data['ContactName'], Address=data['Address'], City = data['City'],  PostalCode=data['PostalCode'], Country=data['Contry'])
+    result = do.Customer(ConnectionData).update(c)
+    return jsonify({'message': result[0]}),result[1]
     
 #Show some row by ID
-@app.route('/user/get/<int:user_id>')
+@app.route('/customer/get/<int:user_id>')
 def get_user_by_id(user_id):
     c = bo.Customer(CustomerID = user_id)
     result = do.Customer(ConnectionData).get_by_id(c)
@@ -65,15 +82,7 @@ def user_get_by_id():
     result['user_id'] = 1
     return jsonify(result), 200
 
-@app.route('/user/insert', methods=['POST'])
-def user_insert():
-    data = request.json
-    c1 = bo.Customer(data['CustomerID'], data['CustomerName'], data['ContactName'], data['Address'], data['City'], data['PostalCode'], data['Country'])
-    c2 = do.Customer(ConnectionData)
-    s1 = c2.insert(c1)
-    result = {}
-    result['message'] = s1
-    return jsonify(result), 200
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
